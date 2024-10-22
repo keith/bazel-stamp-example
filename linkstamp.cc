fa625d69ffe4223c113bacffbe5a0a5978a80332
Keith Smiley <keithbsmiley@gmail.com>
2024-10-22 10:53:56 -0700

    Add comment about variable name

--- linkstamp.cc
+++ linkstamp.cc
@@ -3,4 +3,5 @@ extern const char build_scm_revision[];
 
 // Implicit and always available https://bazel.build/docs/user-manual#workspace-status
 const char build_host[] = BUILD_HOST;
+// This has to be BUILD_SCM_REVISION https://github.com/bazelbuild/bazel/issues/4863
 const char build_scm_revision[] = BUILD_SCM_REVISION;
