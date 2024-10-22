938609b13f71ec99cf8275fc553342ee7b8be1c8
Keith Smiley <keithbsmiley@gmail.com>
2024-10-22 10:47:25 -0700

    Add BUILD_HOST example
    Same as sha but it comes from the stable status

--- linkstamp.cc
+++ linkstamp.cc
@@ -1,3 +1,6 @@
+extern const char build_host[];
 extern const char build_scm_revision[];
 
+// Implicit and always available https://bazel.build/docs/user-manual#workspace-status
+const char build_host[] = BUILD_HOST;
 const char build_scm_revision[] = BUILD_SCM_REVISION;
--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
+extern const char build_host[];
 extern const char build_scm_revision[];
 
 int main() {
   printf("Built at sha: %s!\n", build_scm_revision);
+  printf("Built with host: %s!\n", build_host);
   return 0;
 }
